73659d47e95c1c36269f011ac3209f2b7d2b6576
eevajonnapanula <eevajonna.panula@gmail.com>
2021-03-11 11:18:28 +0000

    Deploying to gh-pages from  @ 1a3ad9337994170b7455c860f57bd1e04bb689c3 🚀

---
+++ _next/static/_D-QBm5MaL3B47-D24zpd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-fee16c77d9a603e54f8e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-986b9ac52257fbcb5ebd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
